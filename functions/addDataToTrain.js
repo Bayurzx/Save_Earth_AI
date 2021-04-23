a881569542f47d1f355f1c731f168fd32015084d
Bayurzx <bayurzx@gmail.com>
2021-04-23 16:33:50 +0100

    Updated the code for datastax astra

--- functions/addDataToTrain.js
+++ functions/addDataToTrain.js
@@ -7,13 +7,12 @@ const myuuid = JSON.stringify(cassandra.types.TimeUuid.now());
 const collection = 'trainingdata'
 
 
-exports.handler = async function (event, context, callback) {
-  const astraClient = await createClient({
-      astraDatabaseId: process.env.ASTRA_DB_ID,
-      astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-      username: process.env.ASTRA_DB_USERNAME,
-      password: process.env.ASTRA_DB_PASSWORD,
-  })
+exports.handler = async (event, context, callback) => {
+    const astraClient = await createClient({
+        astraDatabaseId: process.env.ASTRA_DB_ID,
+        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+        applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+    })
 
   const posts = astraClient
       .namespace(process.env.ASTRA_DB_KEYSPACE)
--- functions/thanks.js
+++ functions/thanks.js
@@ -7,12 +7,11 @@ const myuuid = JSON.stringify(cassandra.types.TimeUuid.now());
 const collection = 'thanks'
 
 
-exports.handler = async function (event, context, callback) {
+exports.handler = async (event, context, callback) => {
     const astraClient = await createClient({
         astraDatabaseId: process.env.ASTRA_DB_ID,
         astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-        username: process.env.ASTRA_DB_USERNAME,
-        password: process.env.ASTRA_DB_PASSWORD,
+        applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
     })
 
     const posts = astraClient
